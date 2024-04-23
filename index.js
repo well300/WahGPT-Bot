26304f38534f95e618a7444e01606c42a75118c2
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2024-04-24 01:06:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,183 +1,56 @@
-import {
-    jidDecode,
-    makeWASocket,
-    DisconnectReason,
-    useMultiFileAuthState,
-    fetchLatestBaileysVersion
-} from '@whiskeysockets/baileys'
-import {
-    Boom
-} from '@hapi/boom'
+import ws from '@whiskeysockets/baileys'
+const { default: makeWASocket, DisconnectReason, useMultiFileAuthState, fetchLatestBaileysVersion } = ws
+import { remove } from 'fs-extra'
+import { serialize } from './lib/waClient.js'
 import * as dotenv from 'dotenv'
 import axios from 'axios'
 import P from 'pino'
+import { Boom } from '@hapi/boom'
 
 dotenv.config()
 
-connect()
-
-const getChatGPTResponse = async (text) => {
-    const apiUrl = `${process.env.CHATGPT_API_URL}?prompt=${encodeURIComponent(text)}`
-    try {
-        const response = await axios.get(apiUrl)
-        if (response.data.status) {
-            return response.data.result.reply
-        } else {
-            throw new Error('API error')
-        }
-    } catch (error) {
-        console.error(
-            chalk.red(`❌ Error calling ChatGPT API: ${error.message}`)
-        )
-        throw new Error('Error calling ChatGPT API')
-    }
-}
-
-const getDALLEImage = async (text) => {
-    const apiUrl = `${process.env.DALLE_API_URL}?q=${encodeURIComponent(text)}`
-    try {
-        const response = await axios.get(apiUrl)
-        const buffer = (
-            await axios.get(response.data.result, {
-                responseType: 'arraybuffer'
-            })
-        ).data
-        return buffer
-    } catch (error) {
-        console.error(
-            chalk.red(`❌ Error calling DALL·E API: ${error.message}`)
-        )
-        throw new Error('Error calling DALL·E API')
-    }
-}
-
-const sanitizeJids = (jid) => {
-    if (/:\d+@/gi.test(jid)) {
-        const decoded = jidDecode(jid)
-        if (decoded?.server && decoded.user) {
-            return format('%s@%s', decoded.user, decoded.server)
-        }
-        return jid
-    } else return jid
-}
-
-const connect = async () => {
-    const {
-        state,
-        saveCreds
-    } = await useMultiFileAuthState('session')
-    const socket = makeWASocket({
+const start = async () => {
+    const { state, saveCreds } = await useMultiFileAuthState("session")
+    const client = makeWASocket({
         version: (await fetchLatestBaileysVersion()).version,
         auth: state,
-        logger: P({
-            level: 'silent'
-        }),
+        logger: P({ level: 'silent' }),
         printQRInTerminal: true
     })
-    socket.ev.on('connection.update', async (update) => {
-        const {
-            connection,
-            lastDisconnect,
-            qr
-        } = update
-        const {
-            statusCode
-        } = new Boom(lastDisconnect?.error).output
-        if (qr) console.log('Qr has been generated!!')
 
+    //connection updates
+    client.ev.on('connection.update', async (update) => {
+        const { connection, lastDisconnect } = update
+        if (update.qr) console.log(`Scan the QR code!!`)
         if (connection === 'close') {
-            if (statusCode !== DisconnectReason.loggedOut)
-                setTimeout(() => connect(), 3000)
+            const { statusCode } = new Boom(lastDisconnect?.error).output
+            if (statusCode !== DisconnectReason.loggedOut) setTimeout(() => start(), 3000)
             else {
-                console.log(
-                    'Disconnected! Something went wrong during connection!'
-                )
-                setTimeout(() => connect(), 3000)
+                console.log('Disconnected :"(')
+                await remove("session")
+                console.log('Starting...')
+                setTimeout(() => start(), 3000)
             }
         }
-        if (connection === 'connecting') console.log('Connecting to the phone!')
-        if (connection === 'open') console.log('Connected to the phone >.<!')
+        if (connection === 'connecting') console.log('Connecting to WhatsApp!!')
+        if (connection === 'open') console.log('Connected to WhatsApp')
+        
     })
-    socket.ev.on('messages.upsert', async ({
-        messages
-    }) => {
-        const {
-            message
-        } = JSON.parse(JSON.stringify(messages[0]))
-        const content = (() => {
-            if (message?.buttonsResponseMessage)
-                return message?.buttonsResponseMessage?.selectedButtonId || ''
-            if (message?.listResponseMessage)
-                return (
-                    message?.listResponseMessage?.singleSelectReply
-                    ?.selectedRowId || ''
-                )
-            return message?.conversation ?
-                message.conversation :
-                this.supportedMediaMessages.includes(type) ?
-                this.supportedMediaMessages
-                .map((type) => message?.[type]?.caption)
-                .filter((caption) => caption)[0] || '' :
-                message?.extendedTextMessage?.text ?
-                message?.extendedTextMessage.text :
-                ''
-        })()
-        const array =
-            (message?.[type]?.contextInfo?.mentionedJid ? message[type]?.contextInfo?.mentionedJid : []) || []
-        const mentioned = array.filter(Boolean)
-        if (quoted?.sender) M.mentioned.push(M.quoted.sender)
-        if (mentioned.includes(sanitizeJid(socket.user?.id ?? '')) && !content.startsWith(process.env.PREFIX)) {
-            try {
-                const text = await getChatGPTResponse(content)
-                return socket.sendMessage(
-                    M.from, {
-                        text
-                    }, {
-                        quoted: M
-                    }
-                )
-            } catch (err) {
-                socket.sendMessage(
-                    M.from, {
-                        text: err.message
-                    }, {
-                        quoted: M
-                    }
-                )
-            }
+    client.ev.on('messages.upsert', async ({ messages, type }) => {
+        if (type !== 'notify') return
+        const M = serialize(JSON.parse(JSON.stringify(messages[0])), client)
+        if (M.quoted?.participant) M.mentions.push(M.quoted.participant)
+        console.log(M.body)
+        if (
+            M.mentions.includes(client.user.id.split(':')[0] + '@s.whatsapp.net')
+        ) {
+            const text = await axios.get(`https://oni-chan-unique-api.vercel.app/gpt4?text=${M.body}`)
+            M.reply(text.data.result)
         }
 
-        const [cmd, ...args] = body.replace(process.env.PREFIX, '').split(' ')
-        const arg = args.join(' ')
-        switch (cmd) {
-            case 'dalle':
-                if (!arg)
-                    return socket.sendMessage(
-                        M.from, {
-                            text: 'You did not provide any term!!'
-                        }, {
-                            quoted: M
-                        }
-                    )
-                try {
-                    const image = await getDALLEImage(arg)
-                    return socket.sendMessage(
-                        M.from, {
-                            image
-                        }, {
-                            quoted: M
-                        }
-                    )
-                } catch (err) {
-                    socket.sendMessage(
-                        M.from, {
-                            text: err.message
-                        }, {
-                            quoted: M
-                        }
-                    )
-                }
-        }
+
     })
-    socket.ev.on('creds.update', saveCreds)
+    client.ev.on('creds.update', saveCreds)
 }
+
+start()
